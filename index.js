56bca1706da62de6192de36af6d2e1a8a8c5a794
gamersindo1223 <76719536+gamersindo1223@users.noreply.github.com>
2022-12-09 12:54:47 +0000

    a

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const express = require('express')
-const router = express.Router()
+const app = express()
 const cors = require('cors')
 const path = require('path')
 const serverless = require('serverless-http');
-const app = express()
 const port = 25727 
-router.use(cors())
-router.get("/", async(req,res) =>{
+app.use(cors())
+app.get("/", async(req,res) =>{
   res.sendFile(path.join(__dirname + "/Website/index.html"))
 })
-router.get("/docs", async (req, res) => {
+app.get("/docs", async (req, res) => {
   res.send('<meta http-equiv="refresh" content="0; URL=https://mrgamers11223s-organization.gitbook.io/docs/"/>')
 })
-router.get("*", async (req, res) => {
+app.get("*", async (req, res) => {
   res.status(404).sendFile(path.join(__dirname + "/public/404.html"))
 })
 
 
 app.listen(port, () => {
-  console.log(`Example router listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`)
   console.log("Press Ctrl+C to quit.")
 })
 
